7b5ac4ee67b2ab89acb5117fe15e4772c81e3512
Choka Thenappan <choka@Chokas-MacBook-Air.local>
2024-04-15 14:55:25 -0400

    Function Interface

--- C_model/hw_sim.c
+++ C_model/hw_sim.c
@@ -1,1 +1,23 @@
 // This is the exchange file which takes the Stream nasdaq
+typedef struct book_entry{
+    int price;
+    int order_id;
+    int quantity;
+};
+
+typedef struct hw_output{
+    int best_price_0;
+    int best_price_1;
+    int best_price_2;
+    int best_price_3;
+    int size_of_stock_0;
+    int size_of_stock_1;
+    int size_of_stock_2;
+    int size_of_stock_3;
+};
+
+
+ int hw_sim(struct book_entry order_to_add, int stock_id, int quantity, int request, int order_id){
+
+
+ }
