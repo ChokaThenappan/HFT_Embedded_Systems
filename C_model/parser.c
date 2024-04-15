c47d2bdde4972e541125586e9cd23a1a62d2a9a9
ss6960 <ss6960@micro29.ee.columbia.edu>
2024-04-15 16:02:50 -0400

    init parser file

--- C_model/parser.c
+++ C_model/parser.c
@@ -1,13 +1,54 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <netinet/in.h>
 
-
-// take data from nasdaq ITHC protocol and return to the hardware_sim.c file
-
-
-
-int main() 
+typedef struct 
 {
-	printf("C Module");
+	char msg_type;
+	uint32_t stock_addr;
+	uint32_t stock_id;
+	uint32_t timestamp;
+	int req_type[3] = {0,1,2};
+	int qty;
+	uint32_t order_id;
+	char Stock[8];
+} Stock_Directory_Message;
+
+void parse_stock_directory_message(const char* buffer, Stock_Directory_Message* message) 
+{
+	int offset = 0;
+
+	message->msg_type = buffer[offset];
+	offset += sizeof(char);
+
+	message->stock_addr = ntohl(*(uint32_t*)(buffer+offset));
+	offset += sizeof(uint32_t);
+	
+	message->stock_id = ntohl(*(uint32_t*)(buffer+offset));
+	offset += sizeof(uint32_t);
+
+	message->timestamp;
+	offset += sizeof(uint64_t);
+
+	message->req_type;
+	// offset
+
+	message->qty;
+	// offset
+	
+	message->order_id;
+	// offset
+	
+	message->Stock;
+	// offset
+	
+	message->stock_id;
+	// offset
 }
 
-return 0;
+int main()
+{
+	printf("Hello, world!");
+	return 0;
+}
+
