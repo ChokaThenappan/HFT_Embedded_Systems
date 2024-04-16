b1388b9dba93bb982e83cf4c12e870f40e1bd374
Choka Thenappan <choka@Chokas-MacBook-Air.local>
2024-04-16 00:38:59 -0400

    12:35

--- C_model/hw_sim.c
+++ C_model/hw_sim.c
@@ -206,7 +206,9 @@ int hw_sim(struct book_entry order_to_add, int stock_id, int quantity, int reque
                 return add_order0(order_to_add, stock_id, prev_price.size_of_stock_0, order_id, prev_price.best_price_0);
             }
             else if (stock_id == 1){
-                return add_order1(order_to_add, stock_id, prev_price.size_of_stock_1, order_id, prev_price.best_price_1);
+                add_order1(order_to_add, stock_id, prev_price.size_of_stock_1, order_id, prev_price.best_price_1);
+                //prev_price.size_of_stock_1 = prev_price.size_of_stock_1+1;
+                return 1;
             }
             else if (stock_id == 2){
                 return add_order2(order_to_add, stock_id, prev_price.size_of_stock_2, order_id, prev_price.best_price_2);
@@ -233,6 +235,40 @@ int hw_sim(struct book_entry order_to_add, int stock_id, int quantity, int reque
 int main() {
     struct book_entry order = {1, 1, 1};
     struct hw_output prev_price = {0}; // Assuming initial values are zero
+    hw_sim(order, 1, 1, 0, 1, prev_price);
+    prev_price.size_of_stock_1 = prev_price.size_of_stock_1 + 1; // Example call
+    order.quantity = 2;
+    printf("%d  \n" , prev_price.size_of_stock_1);
     hw_sim(order, 1, 1, 0, 1, prev_price); // Example call
+    order.quantity = 3;
+    prev_price.size_of_stock_1 = prev_price.size_of_stock_1 + 1;
+    hw_sim(order, 1, 1, 0, 1, prev_price);
+    order.quantity = 25;
+    prev_price.size_of_stock_1 = prev_price.size_of_stock_1 + 1;
+    hw_sim(order, 1, 1, 0, 1, prev_price);
+    prev_price.size_of_stock_1 = prev_price.size_of_stock_1 + 1;
+    hw_sim(order, 1, 3, 1, 1, prev_price); // Example call
+
+    printf(" Stock 0: Order ID | Price | Quantity \n");
+    for (int i =0 ; i < 5 ; i++ ){
+        printf("              %d    |    %d    |    %d \n", mem0 [0][i], mem0 [1][i] , mem0 [2][i]);
+    }
+
+    printf(" Stock 1:  Order ID | Price | Quantity \n");
+    for (int i =0 ; i < 5 ; i++ ){
+        printf("              %d    |    %d    |    %d \n", mem1 [0][i], mem1 [1][i] , mem1 [2][i]);
+    }
+
+    printf(" Stock 2:  Order ID | Price | Quantity \n");
+    for (int i =0 ; i < 5 ; i++ ){
+        printf("              %d    |    %d    |    %d \n", mem2 [0][i], mem2 [1][i] , mem2 [2][i]);
+    }
+
+    printf(" Stock 3:  Order ID | Price | Quantity \n");
+    for (int i =0 ; i < 5 ; i++ ){
+        printf("              %d    |    %d    |    %d \n", mem3 [0][i], mem3 [1][i] , mem3 [2][i]);
+    }
+
+
     return 0;
 }
