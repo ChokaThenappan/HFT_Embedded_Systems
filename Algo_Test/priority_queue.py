0c496ccb6f28b504e897c837d3f87965c89d3147
shvam0000 <shvam0000@gmail.com>
2024-04-21 18:53:14 -0400

    add system clock and time analysis

--- Algo_Test/priority_queue.py
+++ Algo_Test/priority_queue.py
@@ -1,12 +1,5 @@
-"""
-Taking the data type as Arrays
-
-Implementing - 
-    1. Insertion (enqueue)
-    2. Getting best price (peek_max)
-"""
-
 import sys
+import time
 
 class message:
     price = 0
@@ -18,13 +11,20 @@ class order_book:
 
     size = -1
 
+    @staticmethod
     def add_order(price, priority):
+        start_time = time.time()
+
         order_book.size += 1
 
         order_book.pr[order_book.size] = message()
         order_book.pr[order_book.size].price = price 
         order_book.pr[order_book.size].priority = priority
+
+        end_time = time.time()
+        print(f"Time taken to add_order: {end_time - start_time} seconds")
     
+    @staticmethod
     def peek():
         highestPriority = -sys.maxsize
         ind = -1
@@ -39,29 +39,37 @@ class order_book:
                 ind = i
             i += 1
 
-            return ind
+        return ind
 
+    @staticmethod
     def peek_max():
-        """
-        peek for the max price in the data structure
-        """
-        pass
+        start_time = time.time()
 
+        temp_max = -sys.maxsize
+        for i in range(order_book.size + 1):  # Adjusted to include the last element
+            if (temp_max < order_book.pr[i].price):
+                temp_max = order_book.pr[i].price
+        
+        end_time = time.time()
+        print(f"Time taken to peek_max: {end_time - start_time} seconds")
+        return temp_max
 
+    @staticmethod
     def main(args):
         print("Main function")
-        order_book.add_order(10, 4)
-        order_book.add_order(14, 4)
-        order_book.add_order(16, 4)
+        order_book.add_order(10, 1)
+        order_book.add_order(14, 2)
+        order_book.add_order(16, 3)
         order_book.add_order(12, 4)
 
-        print(order_book.size)
-        
-        ind = order_book.peek()
-        print(order_book.pr[ind].price)
+        # Print current size
+        print(f"Current size of order_book: {order_book.size + 1}")
 
-                                 
+        # ind = order_book.peek()
+        # print(order_book.pr[ind].price)
+        # print(order_book.pr[ind].priority)
+        res = order_book.peek_max()
+        print(f"Max price in order_book: {res}")
+
 if __name__ == "__main__":
-    # Change this to the function name
     order_book.main([])
-
