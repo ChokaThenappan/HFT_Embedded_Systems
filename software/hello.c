3727a385889c2e78410b86768f1d71c4124fc53b
shvam0000 <shvam0000@gmail.com>
2024-05-11 23:23:22 -0400

    debug

--- software/hello.c
+++ software/hello.c
@@ -100,7 +100,7 @@ void write_message(const vga_ball_color_t *c) {
     unsigned char bufferNotEmpty = ioctl(vga_ball_fd, VGA_BALL_READ_DATA, &vla);
     unsigned char readPort = ioctl(vga_ball_fd, VGA_BALL_READ_DATA, &vla);
 
-    // if (bufferNotEmpty && readPort) {
+    if (bufferNotEmpty && readPort) {
 
     if (ioctl(vga_ball_fd, VGA_BALL_WRITE_DATA, &vla)) {
         printf("Data written to device:\n");
@@ -112,15 +112,15 @@ void write_message(const vga_ball_color_t *c) {
         return;
     }
 
-    // }
-    // else {
-    //     printf("Waiting for buffer and port to be ready...\n");
-    //     printf("Data written to device:\n");
-    //     printf("Msg Type: %02x, Timestamp: %02x, Order Ref Number: %02x, Trans ID: %02x, Order Book ID: %02x, Side: %02x, Qty: %02x, Price: %02x, Yield: %02x\n",
-    //        vla.message.msg_type, vla.message.timestamp, vla.message.order_ref_number,
-    //        vla.message.trans_id, vla.message.order_book_id, vla.message.side,
-    //        vla.message.qty, vla.message.price, vla.message.yield);
-    // }
+    }
+    else {
+        printf("Waiting for buffer and port to be ready...\n");
+        printf("Data written to device:\n");
+        printf("Msg Type: %02x, Timestamp: %02x, Order Ref Number: %02x, Trans ID: %02x, Order Book ID: %02x, Side: %02x, Qty: %02x, Price: %02x, Yield: %02x\n",
+           vla.message.msg_type, vla.message.timestamp, vla.message.order_ref_number,
+           vla.message.trans_id, vla.message.order_book_id, vla.message.side,
+           vla.message.qty, vla.message.price, vla.message.yield);
+    }
 }
 
 void *network_thread_f(void *arg) {
